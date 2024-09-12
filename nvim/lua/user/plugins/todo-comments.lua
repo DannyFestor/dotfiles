9376c48c3d9127eaa57b4a9a65a63399f797d527
Danny Festor <danny.festor@gmail.com>
2024-09-12 11:58:55 +0900

    Remove nvim config for fresh start

--- nvim/lua/user/plugins/todo-comments.lua
+++
@@ -1,9 +0,0 @@
--- Highlight todo, notes, etc in comments
-local opts = { signs = false }
-
-return {
-	"folke/todo-comments.nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = opts,
-}
