56d3af12eb5360ec1fe5551e13d4c4e45afe7108
Danny Festor <danny.festor@gmail.com>
2024-09-12 14:01:53 +0900

    Add Treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,4 +17,5 @@ require("lazy").setup({
     require 'plugins.colortheme',
     require 'plugins.bufferline',
     require 'plugins.lualine',
+    require 'plugins.treesitter',
 })
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,142 @@
+return { -- Highlight, edit, and navigate code
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  opts = {
+    ensure_installed = {
+      'go',
+      'php',
+      'rust',
+      'lua',
+      'python',
+      'javascript',
+      'typescript',
+      'vimdoc',
+      'vim',
+      'regex',
+      'sql',
+      'dockerfile',
+      -- 'toml',
+      'json',
+      'java',
+      -- 'groovy',
+      'gitignore',
+      'graphql',
+      'yaml',
+      'make',
+      'cmake',
+      'markdown',
+      'markdown_inline',
+      'bash',
+      'tsx',
+      'css',
+      'html',
+    },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+      --  If you are experiencing weird indenting issues, add the language to
+      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+  },
+  -- There are additional nvim-treesitter modules that you can use to interact
+  -- with nvim-treesitter. You should go explore a few and see what interests you:
+  --
+  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  dependencies = {
+	"nvim-treesitter/nvim-treesitter-context",
+	"nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  config = function (_, opts)
+	-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+	---@diagnostic disable-next-line: missing-fields
+	require("nvim-treesitter.configs").setup(opts)
+
+	-- There are additional nvim-treesitter modules that you can use to interact
+	-- with nvim-treesitter. You should go explore a few and see what interests you:
+	--
+	--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+	--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+	--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+
+	require("nvim-treesitter.configs").setup({
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<a-up>",
+				node_incremental = "<a-up>",
+				scope_incremental = "<a-s>",
+				node_decremental = "<a-down>",
+			},
+		},
+
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["aa"] = "@parameter.outer",
+					["ia"] = "@parameter.inner",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+			swap = {
+				enable = true,
+				swap_previous = {
+					["<leader>A"] = "@parameter.inner",
+				},
+				swap_next = {
+					["<leader>a"] = "@parameter.inner",
+				},
+			},
+		},
+	})
+
+	require("treesitter-context").setup({
+		enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+		max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+		min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+		line_numbers = true,
+		multiline_threshold = 20, -- Maximum number of lines to show for a single context
+		mode = "cursor", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+		trim_scope = "outer", -- Line used to calculate context. Choices: 'cursor', 'topline'
+		-- Separator between context and content. Should be a single character string, like '-'.
+		-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+		separator = nil,
+		zindex = 20, -- The Z-index of the context window
+		on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+	})
+  end
+}
