a427bfa41408a705f357ba1b28a07997d38f22be
Danny Festor <danny.festor@gmail.com>
2025-01-08 18:49:53 +0900

    WIP

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,4 +1,17 @@
 return {
 	-- Hints keybinds
 	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		preset = "modern",
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
