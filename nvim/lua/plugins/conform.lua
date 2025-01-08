fd3dedde7750262243b4617e47c333b060239de0
Danny Festor <danny.festor@gmail.com>
2025-01-08 19:14:45 +0900

    WIP

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,12 +3,14 @@
 ---@return string
 local function first(bufnr, ...)
 	local conform = require("conform")
+
 	for i = 1, select("#", ...) do
 		local formatter = select(i, ...)
 		if conform.get_formatter_info(formatter, bufnr).available then
 			return formatter
 		end
 	end
+
 	return select(1, ...)
 end
 
